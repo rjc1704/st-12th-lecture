d71fa517096310b70a1beb6c81b4f0f015fee516
Jecheon Ryu <rjc1704@gmail.com>
2024-06-17 09:32:43 +0900

    enabled Disabling/Pausing Queries

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
 
 export default function TodoList() {
   const navigate = useNavigate();
+
   const {
-    isPending,
+    isFetching,
     error,
     data: todos,
+    refetch,
   } = useQuery({
     queryKey: ["todos"],
     queryFn: async () => {
@@ -15,10 +18,15 @@ export default function TodoList() {
       const response = await todoApi.get("/todos?_sort=-createdAt");
       return response.data;
     },
+    enabled: false,
   });
 
-  if (isPending) {
-    return <div style={{ fontSize: 36 }}>로딩중...</div>;
+  if (isFetching) {
+    return (
+      <div style={{ fontSize: 36 }}>
+        <p>로딩중...</p>
+      </div>
+    );
   }
 
   if (error) {
@@ -28,22 +36,25 @@ export default function TodoList() {
     );
   }
   return (
-    <ul style={{ listStyle: "none", width: 250 }}>
-      {todos.map((todo) => (
-        <li
-          key={todo.id}
-          style={{
-            border: "1px solid black",
-            padding: "10px",
-            marginBottom: "10px",
-          }}
-        >
-          <h3>{todo.title}</h3>
-          <button onClick={() => navigate(`/detail/${todo.id}`)}>
-            내용보기
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <button onClick={refetch}>투두리스트 refetch</button>
+      <ul style={{ listStyle: "none", width: 250 }}>
+        {todos?.map((todo) => (
+          <li
+            key={todo.id}
+            style={{
+              border: "1px solid black",
+              padding: "10px",
+              marginBottom: "10px",
+            }}
+          >
+            <h3>{todo.title}</h3>
+            <button onClick={() => navigate(`/detail/${todo.id}`)}>
+              내용보기
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
